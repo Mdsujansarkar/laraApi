ca5fd9327f6b33c126d935c140f9b28620068e6d
Mdsujansarkar <sujansarkar786@gmail.com>
2020-06-28 04:12:39 +0600

    api first commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/products','ProductController');
+
+Route::group(['prefix'=>'products'], function(){
+	Route::apiResource('/{product}/reviews','ReviewController');
+
 });
